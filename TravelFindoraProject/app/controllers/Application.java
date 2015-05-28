f42c88e5ad7a8d7e80b2425894a99e49f29f8f9c
brazousd <daryabraz@google.com>
2015-05-28 21:53:23 +0200

    olo

--- TravelFindoraProject/app/controllers/Application.java
+++ TravelFindoraProject/app/controllers/Application.java
@@ -1,9 +1,39 @@
 package controllers;
 
 import play.mvc.Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import models.User;
 
 public class Application extends Controller {
+
+    public static void signIn(){
+        render();
+    }
+
     public static void index() {
         render();
     }
+
+    public static void login() {
+        render();
+    }
+
+    public static void authenticate(String username, String password) {
+        boolean auth;
+        auth = (ControllerUser.connect(username, password));
+        render();
+    }
+
+    public static void register(String firstname, String lastname, String email, String password) {
+    User newUser = new User();
+    newUser.setFirstname(firstname);
+    newUser.setLastname(lastname);
+    newUser.setEmail(email);
+    newUser.setPassword(password);
+
+    newUser.save();
+    }
+
 }
---
+++ TravelFindoraProject/app/controllers/CommentaireTravels.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import play.*;
+
+public class CommentaireTravels extends CRUD{
+
+}
