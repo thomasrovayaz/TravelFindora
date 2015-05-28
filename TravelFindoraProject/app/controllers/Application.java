c1acc4b526c0f60077c51f9c22a9ea43d3bd929e
Alexia Arnaud <zappygirl9@hotmail.fr>
2015-05-28 15:19:14 +0200

    register new member

--- TravelFindoraProject/app/controllers/Application.java
+++ TravelFindoraProject/app/controllers/Application.java
@@ -1,8 +1,16 @@
 package controllers;
 
 import play.mvc.Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import models.User;
 
 public class Application extends Controller {
+
+    public static void signIn(){
+        render();
+    }
 
     public static void index() {
         render();
@@ -16,6 +24,16 @@ public class Application extends Controller {
         boolean auth;
         auth = (ControllerUser.connect(username, password));
         render();
+    }
+
+    public static void register(String firstname, String lastname, String email, String password) {
+    User newUser = new User();
+    newUser.setFirstname(firstname);
+    newUser.setLastname(lastname);
+    newUser.setEmail(email);
+    newUser.setPassword(password);
+
+    newUser.save();
     }
 
 }
