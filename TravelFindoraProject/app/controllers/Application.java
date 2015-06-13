b57a84f24756121337665e369174bebdcc398f4c
thomas <tho@gmail.com>
2015-06-13 09:48:43 +0200

    commit

--- TravelFindoraProject/app/controllers/Application.java
+++ TravelFindoraProject/app/controllers/Application.java
@@ -26,54 +26,54 @@ public class Application extends Controller {
     }
 
     public static void register(String firstname, String lastname, String email, String password) {
-    User newUser = new User();
-    newUser.setFirstname(firstname);
-    newUser.setLastname(lastname);
-    newUser.setEmail(email);
-    newUser.setPassword(password);
+        User newUser = new User();
+        newUser.setFirstname(firstname);
+        newUser.setLastname(lastname);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
 
         newUser.save();
 
         session.put("email",email);
         Application.index();
     }
-    
+
     public static void search(String name){
-    	Findora f = Findora.find("name = ?", name).first();
-    	if(f!=null){
-        Set<TravelFindora> tfs = f.getTravels();
-        List <Travel> ts = new ArrayList<Travel>();
-        for(TravelFindora t: tfs){
-        	ts.add(t.getTravel());
+        Findora f = Findora.find("byName", name).first();
+        if(f!=null){
+            Set<TravelFindora> tfs = f.getTravels();
+            List <Travel> ts = new ArrayList<Travel>();
+            for(TravelFindora t: tfs){
+                ts.add(t.getTravel());
+            }
+            Set<Integer> userIds = new HashSet<Integer>();
+            //   Set<Integer> likersIds = new HashSet<Integer>();
+            List<Commentaire> com = new ArrayList<Commentaire>();
+
+            for(Travel t: ts){
+                for(TravelUser tu: t.getTravellers())
+                    userIds.add(tu.getTraveller().getUserId());
+                com.addAll(t.getCommentaires());
+
+                //   	for(TravelLike tl: t.getLikers())
+                //   		likersIds.add(tl.getLikerTravel().getUserId());
+            }
+            int nbUsers = userIds.size();
+            //  int likes = likersIds.size();
+
+            Collections.sort(com, new Comparator() {
+                public int compare(Object A, Object B) {
+                    return ((Commentaire)A).getDateCreation()
+                            .compareTo(((Commentaire)B).getDateCreation());
+                }
+            });
+
+            render(name, nbUsers, com);
         }
-        Set<Integer> userIds = new HashSet<Integer>();
-     //   Set<Integer> likersIds = new HashSet<Integer>();
-        List<Commentaire> com = new ArrayList<Commentaire>();
-        
-        for(Travel t: ts){
-        	for(TravelUser tu: t.getTravellers())
-        		userIds.add(tu.getTraveller().getUserId());
-        com.addAll(t.getCommentaires());
-     
-     //   	for(TravelLike tl: t.getLikers())
-     //   		likersIds.add(tl.getLikerTravel().getUserId());
-        }
-        int nbUsers = userIds.size();
-      //  int likes = likersIds.size();
-      
-        Collections.sort(com, new Comparator() {
-        	public int compare(Object A, Object B) {
-        	return ((Commentaire)A).getDateCreation()
-        	.compareTo(((Commentaire)B).getDateCreation());
-        	}
-        	});
-        
-        render(name, nbUsers, com);
-    	}
-    	else render();
+        else render();
     }
-    
+
     public static void searchVal(String query){
-    	search(query);    	
+        search(query);
     }
 }
