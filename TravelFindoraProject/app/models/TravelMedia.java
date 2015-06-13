98a43e33e41e53ea249592afe787c11bf6a412fd
Alexia Arnaud <zappygirl9@hotmail.fr>
2015-06-13 15:25:02 +0200

    git history fixed

--- TravelFindoraProject/app/models/TravelMedia.java
+++ TravelFindoraProject/app/models/TravelMedia.java
@@ -1,33 +1,66 @@
 package models;
 
-import javax.persistence.Entity;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-/** * Created by thomas on 25/05/15. */
-@Entity public class TravelMedia extends Content { private String description; public byte[] file; public String fileName; public String contentType;
-    public byte[] file;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+
+import play.data.validation.MaxSize;
+import play.data.validation.Required;
+import play.db.jpa.Blob;
+import play.libs.MimeTypes;
+
+/**
+ * Created by thomas on 25/05/15.
+ */
+@Entity
+public class TravelMedia extends Content {
+
+
+    @Lob
+    @Required
+    @MaxSize(10000)
+    private String description;
+
+    public Blob file;
     public String fileName;
     public String contentType;
 
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
-
-    public byte[] getFile() { return file; }
-    
-    public byte[] getFile() {
-        return file;
+    public String getDescription() {
+        return description;
     }
 
-    public void setFile(byte[] file) throws FileNotFoundException {
-        this.file = file;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getContentType() { return contentType; }
+    public File getFile() {
+        return file.getFile();
+    }
 
-    public void setContentType(String contentType) { this.contentType = contentType; }
+    public void setFile(File file) throws FileNotFoundException {
+        this.file = new Blob();
+        this.file.set (new FileInputStream(file), MimeTypes.getContentType(file.getName()));
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
 
     public String getFileName() {
 
-        return fileName; }
+        return fileName;
+    }
 
-    public void setFileName(String fileName) { this.fileName = fileName; } }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+}
+
